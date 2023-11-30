8810e35e6b97b8e7e13f9ae0b64039c2f792e5a7
Debasis Nayak <debasishnayak0739@gmail.com>
2023-11-30 20:46:17 +0530

    under process

---
+++ functionality.js
@@ -0,0 +1,3 @@
+#activeCell{
+    padding:2px;
+}
---
+++ script.js
@@ -0,0 +1,32 @@
+const header = document.getElementById("header");
+const sno = document.getElementById("sno");
+const cellContainer = document.getElementById("body-container");
+
+const columns = 26, rows = 50;
+
+for(let i = 1; i <= columns; i++){
+    const headCell = document.createElement("div");
+    headCell.className = "head-cell";
+    headCell.innerText = String.fromCharCode(64 + i);
+    header.appendChild(headCell);
+} 
+
+for(let j = 1; j <= rows; j++){
+    const snoCell = document.createElement("div");
+    snoCell.innerText = j;
+    snoCell.className = "sno-cell";
+    sno.appendChild(snoCell);
+}
+
+for(let row = 1; row <= rows; row++){
+    const rowElement = document.createElement("div");
+    rowElement.className = "row";
+
+    for(let col = 1; col <= columns; col++){
+        const colElement = document.createElement("div");
+        colElement.className = "cell";
+        colElement.contentEditable = true;
+        rowElement.appendChild(colElement);
+    }
+    cellContainer.appendChild(rowElement);
+}
